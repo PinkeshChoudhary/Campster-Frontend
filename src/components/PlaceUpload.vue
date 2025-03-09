2c43ceb92de85afee3b9b37902bda2ffee65a4fa
sachindhatterwal <sachinkumargit17602@gmail.com>
2025-03-10 00:20:31 +0530

    city filter api use

--- src/components/PlaceUpload.vue
+++ src/components/PlaceUpload.vue
@@ -16,7 +16,7 @@
                 <input v-model="place.destination" type="text" placeholder="Destination Name?" class="input-field" required />
             </div>
             <div class="mb-4">
-                <input v-model="place.location" type="text" placeholder="Location " class="input-field" required />
+                <input v-model="place.location" type="text" placeholder="City " class="input-field" required />
             </div>
            
             <div class="mb-4">
@@ -135,7 +135,7 @@ export default {
                     destination: "",
                     description: "",
                     locationCoordinates: "",
-                    City: "",
+                    location: "",
                     images: []
                 };
                 imagePreviews.value = [];
