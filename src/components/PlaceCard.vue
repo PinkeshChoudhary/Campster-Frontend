e1235ac998ff2fe4017a5d8c37ed9e4774bad140
sachindhatterwal <sachinkumargit17602@gmail.com>
2025-03-15 10:33:00 +0530

    destimation name

--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -20,7 +20,7 @@
           <path fill="none" stroke="black" stroke-width="2" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
         </svg>
       </button>
-      <div class="absolute bottom-2 left-2 text-yellow-600 p-0" >More details ...</div>
+      <div class="absolute bottom-2 left-2 text-yellow-600 p-0" >{{ place.destination }}</div>
     </div>
   </div>
 </template>
